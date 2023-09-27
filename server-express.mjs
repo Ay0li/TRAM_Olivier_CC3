ca4269299ec010d3f79f8505327b14619718dbaf
Ay0li <oli66azerty@gmail.com>
2023-09-28 09:40:40 +1100

    Update server-express.mjs

--- server-express.mjs
+++ server-express.mjs
@@ -17,9 +17,14 @@ if (app.get("env") === "development") {
 app.use(express.static("static"));
 
 app.get("/random/:nb", async function (request, response, next) {
-  const length = request.params.nb;
+  const length = Number.parseInt(request.params.nb, 10);
+
+  if (Number.isNaN(length)) {
+    return next(createError(400, 'Invalid number parameter'));
+  }
+
   const numbers = Array.from({ length }).map(() => Math.floor(100 * Math.random()));
-  const welcome = "Welcome to the Random Numbers Page"; 
+  const welcome = "Welcome to the Random Numbers Page"; // Message de bienvenue
 
   response.render("random", { numbers, welcome });
 });
