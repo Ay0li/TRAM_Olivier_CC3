b88e280a44463e682ca88a051340f7c1e8d059c5
Ay0li <oli66azerty@gmail.com>
2023-09-28 11:15:29 +1100

    Update server-express.mjs

--- server-express.mjs
+++ server-express.mjs
@@ -1,14 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 import createError from "http-errors";
+import logger from "loglevel";
 
 const host = "localhost";
 const port = 8000;
 
+logger.setLevel(logger.levels.DEBUG);
 const app = express();
 
 app.set("view engine", "ejs");
-
 
 if (app.get("env") === "development") {
   app.use(morgan("dev"));
@@ -24,18 +25,18 @@ app.get("/random/:nb", async function (request, response, next) {
   }
 
   const numbers = Array.from({ length }).map(() => Math.floor(100 * Math.random()));
-  const welcome = "Welcome to the Random Numbers Page"; // Message de bienvenue
+  const welcome = "Welcome to the Random Numbers Page";
 
   response.render("random", { numbers, welcome });
 });
 
 app.use((request, response, next) => {
-  concole.debug(`default route handler : ${request.url}`);
+  logger.debug(`default route handler : ${request.url}`);
   return next(createError(404));
 });
 
 app.use((error, _request, response, _next) => {
-  concole.debug(`default error handler: ${error}`);
+  logger.debug(`default error handler: ${error}`);
   const status = error.status ?? 500;
   const stack = app.get("env") === "development" ? error.stack : "";
   const result = { code: status, message: error.message, stack };
@@ -45,9 +46,9 @@ app.use((error, _request, response, _next) => {
 const server = app.listen(port, host);
 
 server.on("listening", () =>
-  console.info(
+  logger.info(
     `HTTP listening on http://${server.address().address}:${server.address().port} with mode '${process.env.NODE_ENV}'`,
   ),
 );
 
-console.info(`File ${import.meta.url} executed.`);
+logger.info(`File ${import.meta.url} executed.`);
